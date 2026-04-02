c17a09ed7268e327e84381314cda9bb796bfb04d
Nawat Ruengdechawiwat <nawat0035@gmail.com>
2026-04-02 07:46:33 +0700

    "lab18_1complete"

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -5,8 +5,18 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect r1, Rect r2){
 
+    double xleft   = max(r1.x, r2.x);
+    double xright  = min(r1.x+r1.w, r2.x+r2.w);
+    double ytop    = min(r1.y, r2.y);
+    double ybottom = max(r1.y-r1.h, r2.y-r2.h);
+    double overlapwidth = xright-xleft;
+    double overlapheight = ytop-ybottom;
 
+   if (overlapwidth<= 0 || overlapheight<=0){
+        return 0;
+    }
+    return overlapwidth* overlapheight;
 
 }
